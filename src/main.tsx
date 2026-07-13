d950f14203850b86c55bcc0f12e9e50d042260b6
Parallax Studio <hello@mymindventures.io>
2026-07-13 12:26:11 +0200

    Initialize complete premium Founder Post experience

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { initializeConceptMessageUi } from './lib/conceptMessageUi';
 import { initializeConceptOwnershipUi } from './lib/conceptOwnershipUi';
+import { initializeFounderPostUi } from './lib/founderPostUi';
+import { initializeFounderPostOpportunitiesUi } from './lib/founderPostOpportunitiesUi';
 import { initializeJournalMetadataUi } from './lib/journalMetadataUi';
 import { initializeLatestThreeUi } from './lib/latestThreeUi';
 import { FounderProfilePage } from './pages/FounderProfilePage';
@@ -12,6 +14,8 @@ import './styles/discovery-responsive.css';
 import './styles/conceptOwnership.css';
 import './styles/conceptMessages.css';
 import './styles/founderProfiles.css';
+import './styles/founderPosts.css';
+import './styles/founderPostOpportunities.css';
 import './styles/latestThree.css';
 import './styles/footer.css';
 import './styles/legal.css';
@@ -19,6 +23,8 @@ import './styles/footerLegalPatch.css';
 
 initializeConceptOwnershipUi();
 initializeConceptMessageUi();
+initializeFounderPostUi();
+initializeFounderPostOpportunitiesUi();
 initializeJournalMetadataUi();
 initializeLatestThreeUi();
 
