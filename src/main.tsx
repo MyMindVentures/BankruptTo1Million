cfafc9d1fe18ab2b77112381c2a1586f5ebd5e70
Parallax Studio <hello@mymindventures.io>
2026-07-13 12:01:33 +0200

    Enable Journal metadata and founder profile routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,29 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { initializeConceptMessageUi } from './lib/conceptMessageUi';
 import { initializeConceptOwnershipUi } from './lib/conceptOwnershipUi';
+import { initializeJournalMetadataUi } from './lib/journalMetadataUi';
+import { FounderProfilePage } from './pages/FounderProfilePage';
 import { LegalTransparencyPage } from './pages/LegalTransparencyPage';
 import './styles/global.css';
 import './styles/discovery-responsive.css';
 import './styles/conceptOwnership.css';
 import './styles/conceptMessages.css';
+import './styles/founderProfiles.css';
 import './styles/footer.css';
 import './styles/legal.css';
 import './styles/footerLegalPatch.css';
 
 initializeConceptOwnershipUi();
 initializeConceptMessageUi();
+initializeJournalMetadataUi();
 
-const rootPage = window.location.pathname === '/legal' ? <LegalTransparencyPage /> : <App />;
+const path = window.location.pathname;
+const founderSlug = path.startsWith('/founders/') ? decodeURIComponent(path.split('/')[2] || '') : '';
+const rootPage = path === '/legal'
+  ? <LegalTransparencyPage />
+  : founderSlug
+    ? <FounderProfilePage slug={founderSlug} />
+    : <App />;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
